6f3f99a4cf51b39e998203a9a836b21512461260
drbaph <84208527+Saganaki22@users.noreply.github.com>
2026-04-26 23:37:20 +0100

    Redeploy — loading indicator + handler fix

---
+++ .vite/deps/chunk-CRPP7B4P.js
@@ -0,0 +1,13 @@
+import {
+  CanvasFilterSystem,
+  FilterPipe,
+  FilterSystem
+} from "./chunk-Q4QN7IS3.js";
+import {
+  extensions
+} from "./chunk-UFGTUJO7.js";
+
+// node_modules/pixi.js/lib/filters/init.mjs
+extensions.add(FilterSystem, CanvasFilterSystem);
+extensions.add(FilterPipe);
+//# sourceMappingURL=chunk-CRPP7B4P.js.map
---
+++ .vite/deps/webworkerAll-ERHHNEHA.js
@@ -0,0 +1,8 @@
+import "./chunk-CRPP7B4P.js";
+import "./chunk-Q4QN7IS3.js";
+import "./chunk-W3PHOAVR.js";
+import "./chunk-E5BN425C.js";
+import "./chunk-EFUTS62J.js";
+import "./chunk-S3OCQR2J.js";
+import "./chunk-DSUQR63W.js";
+import "./chunk-UFGTUJO7.js";
